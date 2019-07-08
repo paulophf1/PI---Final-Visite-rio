6e29c8cd595a08624af1cb952bb1ddbd826f4474
paulo <paulohenriquef1@hotmail.com>
2019-07-08 11:45:56 -0300

    final

---
+++ js/index.js
@@ -0,0 +1,97 @@
+
+    $(document).ready(function () {
+
+      // Detecta cliques no botão "home"
+      $('#btn-rio').click(function () {
+        $('.page').hide();
+        $('#rio').show();
+      });
+      // Detecta cliques no botão "contatos"
+      $('#btn-contatos').click(function () {
+        $('.page').hide();
+        $('#contatos').show();
+      });
+      // Detecta cliques no botão "sobre"
+      $('#btn-sobre').click(function () {
+        $('.page').hide();
+        $('#sobre').show();
+      });
+
+
+
+
+
+
+
+      // pagina de cada foto
+      $('.btn-cristo').click(function () {
+        $('.page').hide();
+        $('#cristo').show();
+      });
+      //Maracanã
+      $('.btn-maraca').click(function () {
+        $('.page').hide();
+        $('#maraca').show();
+      });
+      //Jardim Botanico
+      $('.btn-jardim').click(function () {
+        $('.page').hide();
+        $('#jardim').show();
+      });
+      //Teatro Municipal
+      $('.btn-teatro').click(function () {
+        $('.page').hide();
+        $('#teatro').show();
+      });
+      //Copacabana
+      $('.btn-copa').click(function () {
+        $('.page').hide();
+        $('#copa').show();
+      });
+      //Mangaratiba
+      $('.btn-manga').click(function () {
+        $('.page').hide();
+        $('#manga').show();
+      });
+      //Mangaratiba
+      $('.btn-pcopa').click(function () {
+        $('.page').hide();
+        $('#pcopa').show();
+      });
+      //Aquario
+      $('.btn-aqua').click(function () {
+        $('.page').hide();
+        $('#aqua').show();
+      });
+      //Quinta da Boa Vista
+      $('.btn-quinta').click(function () {
+        $('.page').hide();
+        $('#quinta').show();
+      });
+      //Aquario
+      $('.btn-quinta').click(function () {
+        $('.page').hide();
+        $('#quinta').show();
+      });
+      //Pao de Açucar
+      $('.btn-pao').click(function () {
+        $('.page').hide();
+        $('#pao').show();
+      });
+      //cidade olimpica
+      $('.btn-cidade').click(function () {
+        $('.page').hide();
+        $('#cidade').show();
+      });
+      //cidade olimpica
+      $('.btn-museu').click(function () {
+        $('.page').hide();
+        $('#museu').show();
+      });
+
+
+
+
+
+
+    });
